5a72814ea1941ce270cad472d5eb88e4d1229c67
Anton Kulakov <kulakowka@gmail.com>
2017-03-31 01:50:52 +0300

    production config

---
+++ webpack.production.config.js
@@ -0,0 +1,68 @@
+const { resolve } = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  context: resolve(__dirname, 'src'),
+
+  entry: [
+    './client.js'
+  ],
+  output: {
+    filename: 'bundle.js',
+    // the output bundle
+
+    path: resolve(__dirname, 'public'),
+
+    publicPath: '/'
+    // necessary for HMR to know where to load the hot update chunks
+  },
+
+  devtool: 'cheap-module-source-map',
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['es2015', {'modules': false}],
+              'react'
+            ]
+          }
+        }]
+      },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader?modules', 'postcss-loader' ]
+      }
+    ]
+  },
+
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    }),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      beautify: false,
+      mangle: {
+        screw_ie8: true,
+        keep_fnames: true
+      },
+      compress: {
+        screw_ie8: true
+      },
+      compressor: {
+        warnings: false
+      },
+      comments: false
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    })
+  ]
+}
