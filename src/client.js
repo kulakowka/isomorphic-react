ac59a58be242efaafc69da65325c550746fa8faf
Anton Kulakov <kulakowka@gmail.com>
2017-03-31 21:02:26 +0300

    add redux to server and client

--- src/client.js
+++ src/client.js
@@ -1,15 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducers from './reducers'
 import { BrowserRouter } from 'react-router-dom'
 import { AppContainer } from 'react-hot-loader'
 import App from './components/App'
 
+// Grab the state from a global variable injected into the server-generated HTML
+const preloadedState = window.__PRELOADED_STATE__
+
+// Allow the passed state to be garbage-collected
+delete window.__PRELOADED_STATE__
+
+// Create Redux store with initial state
+const store = createStore(reducers, preloadedState)
+
+console.log('state', store.getState())
+
 const render = (Component) => {
   ReactDOM.render(
     <AppContainer>
-      <BrowserRouter>
-        <Component />
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Component />
+        </BrowserRouter>
+      </Provider>
     </AppContainer>,
     document.getElementById('root')
   )
--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 import express from 'express'
 import React from 'react'
 import ReactDOMServer from 'react-dom/server'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducers from './reducers'
 import { StaticRouter } from 'react-router'
 import App from './components/App'
 import { resolve } from 'path'
@@ -22,13 +25,23 @@ function getAssetsPath () {
 function handleRequest (req, res, next) {
   const context = {}
 
+  const counter = parseInt(req.query.counter, 10) || 0
+
+  // Compile an initial state
+  let preloadedState = { counter }
+
+  // Create a new Redux store instance
+  const store = createStore(reducers, preloadedState)
+
   const html = ReactDOMServer.renderToString(
-    <StaticRouter
-      location={req.url}
-      context={context}
-    >
-      <App />
-    </StaticRouter>
+    <Provider store={store}>
+      <StaticRouter
+        location={req.url}
+        context={context}
+      >
+        <App />
+      </StaticRouter>
+    </Provider>
   )
 
   if (context.status) {
@@ -41,17 +54,27 @@ function handleRequest (req, res, next) {
     res.redirect(301, context.url)
   }
 
-  res.end(`
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <title>Example Index</title>
-    </head>
-    <body>
-      <div id="root">${html}</div>
-      ${getAssetsPath()}
-    </body>
+  // Grab the initial state from our Redux store
+  const finalState = store.getState()
+
+  // Send the rendered page back to the client
+  res.send(renderFullPage(html, finalState))
+}
+
+function renderFullPage (html, preloadedState) {
+  return `
+    <!doctype html>
+    <html>
+      <head>
+        <title>Redux Universal Example</title>
+      </head>
+      <body>
+        <div id="root">${html}</div>
+        <script>
+          window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(/</g, '\\u003c')}
+        </script>
+        ${getAssetsPath()}
+      </body>
     </html>
-  `)
+    `
 }
